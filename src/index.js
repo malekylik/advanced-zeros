20287d1cb433a4470856ef064946f9993f1fea0f
malekylik <malekylik@gmail.com>
2018-02-23 21:26:34 +0300

    master/feature: implement the solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var simple = [2,	3,	5,	7,	11,	13,	17,	19,	23,
+const simple = [2,	3,	5,	7,	11,	13,	17,	19,	23,
   	29,	31,	37,	41,	43,	47,	53,	59,	61,	67,	71,
   73,	79,	83,	89,	97,	101,	103,	107,	109,	113,	127,
   	131,	137,	139,	149,	151,	157,	163,	167,	173,
@@ -6,7 +6,6 @@ var simple = [2,	3,	5,	7,	11,	13,	17,	19,	23,
 
 
 module.exports = function getZerosCount(number, base) {
-  if (base === 2) return 0;
   let count = 0;
   let bas;
 
@@ -16,9 +15,21 @@ module.exports = function getZerosCount(number, base) {
       break;
     }
 
+    let cof = 0;
+
+    if((base / bas) % bas === 0){
+      let temp = base / bas;
+      while(temp % bas === 0){ 
+        cof++;
+        temp /= bas;
+      }
+    }
+
   for(let i = bas; i < number; i*=bas){
     count+=Math.floor(number / i);
   }
+
+  if(cof) count = Math.floor(count / (cof + 1)); 
+
   return count;
 }
-
